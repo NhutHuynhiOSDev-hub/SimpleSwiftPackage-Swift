36c22cb8cc66165a55e5455529ccfb87c3bd8016
Huỳnh Quang Nhựt <nhut.hq@seednet.edu.vn>
2021-02-21 11:31:23 +0700

    add cat/ floder

---
+++ cat/Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "cat",
+    dependencies: [
+        
+        .package(name: "FileReader", url: "https://github.com/NhutHuynhiOSDev-hub/SimpleSwiftPackage", from: "1.0.1")
+    ],
+    targets: [
+        
+        .target(
+            name: "cat",
+            dependencies: ["FileReader"]),
+    ]
+)
