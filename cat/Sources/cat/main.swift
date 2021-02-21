f09556aac85b70d55202c17ccadb35f97fc0c02f
Huỳnh Quang Nhựt <nhut.hq@seednet.edu.vn>
2021-02-21 11:37:33 +0700

    blank

--- cat/Sources/cat/main.swift
+++ cat/Sources/cat/main.swift
@@ -1,7 +1,7 @@
 import FileReader
 
 for argument in CommandLine.arguments {
-    
+
     guard argument != "arg1" else { continue }
 
     if let fileContents = FileReader.read(fileName: argument) { print(fileContents) }
